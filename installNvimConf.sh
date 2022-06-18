8d3ca906bd625a0c1234daf001c7c2af553e44aa
Tuhkis <88197425+Tuhkis@users.noreply.github.com>
2022-06-19 01:19:10 +0300

    Update installNvimConf.sh

--- installNvimConf.sh
+++ installNvimConf.sh
@@ -5,6 +5,8 @@ echo Installing...
 mkdir ~/.config/nvim/
 git clone "https://github.com/Tuhkis/nvim-config.git" ~/.config/nvim/
 
+rm ~/.config/nvim/installNvimConf.sh
+
 echo Installing vim-plug.
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
