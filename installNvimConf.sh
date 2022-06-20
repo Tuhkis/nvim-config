86f32e22e3ad7ce7af7d5615b4d17284a34be076
Tuhkis <88197425+Tuhkis@users.noreply.github.com>
2022-06-20 12:51:12 +0300

    Update installNvimConf.sh

--- installNvimConf.sh
+++ installNvimConf.sh
@@ -14,7 +14,6 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 nvim --cmd 'echo Installing plugins.' \
      -c 'PlugInstall' \ # Install plugins
      -c 'qa! # Quit
-# nvim --cmd 'echo Installing coc plugins.' \
-#      -c 'CocInstall coc-discord-rpc' \
-#     -c 'qa!'
-#
+nvim --cmd 'echo Installing coc plugins.' \
+    -c 'CocInstall coc-discord-neovim' \
+    -c 'qa!'
